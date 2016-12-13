5189cf3fe2faad5a618e1eb39383f04f4b59ae23
mirajp <mirage.patel@gmail.com>
2016-12-13 16:27:19 -0500

    Tweaked Actor component to show directors

--- src/routes/Actor/components/Actor/directors.js
+++ src/routes/Actor/components/Actor/directors.js
@@ -1,8 +1,5 @@
 import React from 'react'
-
 import {IndexLink, Link} from 'react-router'
-import {List, ListItem} from 'material-ui'
-
 import './directors.scss'
 
 const Directors = React.createClass({
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import CoreLayout from '../layouts/CoreLayout/CoreLayout'
 import Home from './Home'
 //import CounterRoute from './Counter'
 import ActorRoute from './Actor'
+import MovieRoute from './Movie'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
@@ -13,8 +14,8 @@ export const createRoutes = (store) => ({
   indexRoute  : Home,
   childRoutes : [
     ActorRoute(store)
-    //ActorRoute(store),
-    //CounterRoute(store)
+    , MovieRoute(store)
+    //, CounterRoute(store)
   ]
 })
 
